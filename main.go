65662cdbb089a9bf4374dba8279f2cc54a285c50
aryawirasandi <aryawirasandi7153@gmail.com>
2024-07-13 22:21:56 +0700

    add html interpolation example

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	fs := http.FileServer(http.Dir("assets/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	if tmpl, err := template.ParseFiles("views/template.html"); err != nil {
+		panic("There is not html")
+	} else {
+		data := TodoPageData{
+			PageTitle: "List Of Todo",
+			Todos: []Todo{
+				{
+					Title: "Go to the store",
+					Done:  false,
+				},
+				{
+					Title: "Make some screencast",
+					Done:  true,
+				},
+				{
+					Title: "Get some intermezzo",
+					Done:  false,
+				},
+			},
+		}
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			tmpl.Execute(w, data)
+		})
+	}
+
+	http.ListenAndServe(":3000", nil)
 }
