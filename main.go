3f6909ba020ca4ad7311aca080dab431b76a3a8a
aryawirasandi <aryawirasandi7153@gmail.com>
2024-07-14 19:08:08 +0700

    add json manipulation

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
+func decode(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age)
+}
+
+func encode(w http.ResponseWriter, r *http.Request) {
+	john := User{
+		Firstname: "john",
+		Lastname:  "doe",
+		Age:       42,
+	}
+
+	json.NewEncoder(w).Encode(john)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/decode", decode)
+	http.HandleFunc("/encode", encode)
+
+	http.ListenAndServe(":3000", nil)
 }
