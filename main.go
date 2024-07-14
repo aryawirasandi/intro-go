5d5d6396fcec7af74c668224c370d83d05ab0db2
aryawirasandi <aryawirasandi7153@gmail.com>
2024-07-14 08:37:59 +0700

    add form interpolation

--- main.go
+++ main.go
@@ -45,5 +45,53 @@ func main() {
 		})
 	}
 
+	if tmpl, err := template.ParseFiles("views/forms.html"); err != nil {
+		panic("There is not html")
+	} else {
+		// data := TodoPageData{
+		// 	PageTitle: "List Of Todo",
+		// 	Todos: []Todo{
+		// 		{
+		// 			Title: "Go to the store",
+		// 			Done:  false,
+		// 		},
+		// 		{
+		// 			Title: "Make some screencast",
+		// 			Done:  true,
+		// 		},
+		// 		{
+		// 			Title: "Get some intermezzo",
+		// 			Done:  false,
+		// 		},
+		// 	},
+		// }
+		http.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				tmpl.Execute(w, nil)
+				return
+			}
+
+			type ContatDetails struct {
+				Email   string
+				Subject string
+				Message string
+			}
+
+			details := ContatDetails{
+				Email:   r.FormValue("email"),
+				Subject: r.FormValue("subject"),
+				Message: r.FormValue("message"),
+			}
+
+			result := details
+
+			tmpl.Execute(w, struct {
+				Success bool
+				Result  ContatDetails
+			}{true, result})
+
+		})
+	}
+
 	http.ListenAndServe(":3000", nil)
 }
